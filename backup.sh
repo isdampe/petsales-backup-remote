92746fb23bc32169fc7e63df89a72be65a211888
Richard Denton <isdampe@gmail.com>
2018-12-20 12:09:38 +1100

    First commit

---
+++ backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Type in the domain name you wish to download (e.g. rescuepets.com.au)"
+read domain
+
+mkdir -p ~/Desktop/$domain-backups
+
+if [ -z "$domain" ] 
+then
+	echo "You must provide a domain name."
+	exit 1
+fi
+
+echo "Enter the username"
+read user
+
+if [ -z "$user" ]
+then
+	echo "You must provide a username."
+fi
+
+echo "Going to download from vps.$domain on user $user. Okay? (y/n)"
+read confirm
+if [ ! "y" = "$confirm" ]
+then
+	echo "Cancelling..."
+	exit 1
+fi
+
+out=$domain-$(date +"%Y-%m-%d-%T").tar.gz
+scp $user@vps.$domain:/home/$user/backup/data/*-latest.tar.gz ~/Desktop/$domain-backups/$out
+ls -lth ~/Desktop/$domain-backups
+echo "Finished. Your backup files are located at ~/Desktop/$domain-backups"
